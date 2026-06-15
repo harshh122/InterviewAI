6c4d59e7de2b34b24bdf31482027a7d62dcac536
harshh122 <tiwariharsh472@gmail.com>
2026-06-15 14:27:31 +0530

    Refactor App component for improved readability

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,87 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Auth from './pages/Auth'
-import { useEffect } from 'react'
-import axios from 'axios'
-import { useDispatch } from 'react-redux'
-import { setUserData } from './redux/userSlice'
-import InterviewPage from './pages/InterviewPage'
-import InterviewHistory from './pages/InterviewHistory'
-import Pricing from './pages/Pricing'
-import InterviewReport from './pages/InterviewReport'
+import React, { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+
+import Home from './pages/Home';
+import Auth from './pages/Auth';
+import InterviewPage from './pages/InterviewPage';
+import InterviewHistory from './pages/InterviewHistory';
+import Pricing from './pages/Pricing';
+import InterviewReport from './pages/InterviewReport';
+
+import { setUserData } from './redux/userSlice';
 import ProtectedRoute from "./ProtectedRoute";
 
-export const ServerUrl  = "https://interviewai-0sxf.onrender.com"
+export const ServerUrl = "https://interviewai-0sxf.onrender.com";
 
 function App() {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-  useEffect(()=>{
+  useEffect(() => {
     const getUser = async () => {
       try {
-        const result = await axios.get(ServerUrl + "/api/user/current-user", {withCredentials:true})
-        dispatch(setUserData(result.data))
-      } catch (error) {
-        console.log(error)
-        dispatch(setUserData(null))
-      }
-    }
-    getUser()
+        const result = await axios.get(
+          ServerUrl + "/api/user/current-user",
+          { withCredentials: true }
+        );
 
-  },[dispatch])
+        dispatch(setUserData(result.data));
+      } catch (error) {
+        console.log(error);
+        dispatch(setUserData(null));
+      }
+    };
+
+    getUser();
+  }, [dispatch]);
+
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/auth' element={<Auth/>}/>
-      <Route path='/interview' element={<InterviewPage/>}/>
-      <Route path='/history' element={<InterviewHistory/>}/>
-      <Route path='/pricing' element={<Pricing/>}/>
-      <Route path='/report/:id' element={<InterviewReport/>}/>
 
+      {/* Public Routes */}
+      <Route path='/' element={<Home />} />
+      <Route path='/auth' element={<Auth />} />
 
+      {/* Protected Routes */}
+      <Route
+        path='/interview'
+        element={
+          <ProtectedRoute>
+            <InterviewPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path='/history'
+        element={
+          <ProtectedRoute>
+            <InterviewHistory />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path='/pricing'
+        element={
+          <ProtectedRoute>
+            <Pricing />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path='/report/:id'
+        element={
+          <ProtectedRoute>
+            <InterviewReport />
+          </ProtectedRoute>
+        }
+      />
 
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
